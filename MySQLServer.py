a3d6f4dede4e09abc26f2e084bc9ff1668452b06
Oluwaseyi <oluwaseyipd@gmail.com>
2025-06-25 13:09:57 +0100

    Create database with Python

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,20 +1,26 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="oluwaseyiae",
-    database="alx_book_store"
-)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="oluwaseyiae",
+        database="alx_book_store"
+    )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-sql = "CREATE DATABASE IF NOT EXISTS alx_book_store"
-mycursor.execute(sql)
-print("Database 'alx_book_store' created successfully!")
+    sql = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+    mycursor.execute(sql)
+    print("Database 'alx_book_store' created successfully!")
 
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+finally:
+    if mycursor:
+        print("Cursor is closed.")
+    mycursor.close()
 
-
-mycursor.close()
-mydb.close()
-
+    if mydb:
+        print("MySQL connection is closed.")
+    mydb.close()
